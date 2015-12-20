8d88a3e8c1e3f98803e1dd7fcf3a6349d3cf363b
BridgeLabz <bridgelabz@narayans-macbook-pro-2.local>
2015-12-20 09:36:42 +0530

    Initial Commit

---
+++ colorific/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  colorific
+//
+//  Created by BridgeLabz on 20/12/15.
+//  Copyright © 2015 BridgeLabz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ colorific/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  colorific
+//
+//  Created by BridgeLabz on 20/12/15.
+//  Copyright © 2015 BridgeLabz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
